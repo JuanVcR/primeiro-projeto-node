2bcd00939c4df67df60779f2e41a4f42ccbb12b0
Juan Victor <juanv@pop-os.localdomain>
2021-01-26 14:05:57 -0300

    resolve conflicts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,12 @@
 import { Router } from 'express';
-<<<<<<< HEAD
 import appoitmentsRouter from './appointments.routes';
-
-const routes = Router();
-
-routes.use('/appointments', appoitmentsRouter);
-=======
 import transactionRouter from './transaction.routes';
 
 const routes = Router();
 
+routes.use('/appointments', appoitmentsRouter);
+
 routes.use('/transactions', transactionRouter);
->>>>>>> 4842b80e13e31d25dd3ab363580984b34c38e635
+
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import routes from './routes';
 
@@ -9,10 +8,4 @@ app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
-=======
-import app from './app';
-
-app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
->>>>>>> 4842b80e13e31d25dd3ab363580984b34c38e635
 });
